f6964d54b701db6b1f7419a10f40bbdd0a15ba16
Skylar Brown <skylarmb@gmail.com>
2025-08-14 11:40:04 -0700

    updates

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = false;
+            home-manager.users.x = ./home.nix;
+          }
+        ];
+      };
+    };
+  };
+}
